0417e07f3648f7a447884dd1d06a8ed422d5347e
dcgarzi <dcgarzi@gmail.com>
2026-09-20 22:30:00 -0700

    Update configuration URLs for recommendations

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ window.BOOKCLUB_CONFIG = {
 
   // --- Recommendations (optional; all three come from Google) ---
   // The "Publish to web" CSV link for the form's responses sheet:
-  RECS_CSV_URL: "",
+  RECS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTg1oVjyjw0cLJbA5_LAmmUemmxAJXk-2KEQhDKYgfpEbAE2RbnbqFLxuzFT8vB3gxkVscnWTjIme3k/pub?gid=78340720&single=true&output=csv",
   // The form's share link ("Recommend a book" button on the site):
-  RECS_FORM_URL: "",
+  RECS_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSei9Ai3qmxDr2Gq8kUJbCsXUAHDsx-j3hD_6KTSnkgfpXh6Sw/viewform?usp=header",
   // The sheet's normal edit link (used by admin.html only):
-  RECS_SHEET_URL: "",
+  RECS_SHEET_URL: "https://docs.google.com/spreadsheets/d/1tJeqs7gnmRgLEsLKXMYxYPgYB665Zs9aM3EDE5ygBxI/edit?resourcekey=&gid=78340720#gid=78340720",
 };
 
